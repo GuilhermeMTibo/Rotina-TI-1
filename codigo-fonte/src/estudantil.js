892d303e5634dd367e8d18a6a6547e6d11798248
victorleite06 <victor.leite2001@gmail.com>
2023-05-10 14:50:26 -0300

    Função de create materia funcionando

--- codigo-fonte/src/estudantil.js
+++ codigo-fonte/src/estudantil.js
@@ -1,15 +1,21 @@
-var json = JSON.parse();
-console.log(json)
+var materias = localStorage.getItem('materias') ? JSON.parse(localStorage.getItem('materias')) : []
 
 function salvar() {
     let nome = document.getElementById("nome").value
     let pontos = document.getElementById("pontos").value
     let media = document.getElementById("media").value
     let mat = {
+        id: materias[materias.length - 1] ? materias[materias.length - 1].id + 1 : 1,
         nome: nome, 
         pontos: pontos, 
         media: media
     }
-    json.materias.push(mat)
-    console.log(mat)
+    materias.push(mat)
+    window.location.href = '#'
+
+    localStorage.setItem('materias', JSON.stringify(materias))
+
+    document.getElementById("nome").value = null
+    document.getElementById("pontos").value = null
+    document.getElementById("media").value = null
 }
