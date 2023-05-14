13217987ab4308f5a0a5d0f19b37e805361861a0
victorleite06 <victor.leite2001@gmail.com>
2023-05-14 11:16:31 -0300

    Tentativa de mostrar em tela as matérias cadastradas

--- codigo-fonte/src/estudantil.js
+++ codigo-fonte/src/estudantil.js
@@ -1,5 +1,31 @@
 var materias = localStorage.getItem('materias') ? JSON.parse(localStorage.getItem('materias')) : []
 
+var listaMaterias = document.getElementById("listaMateriasDiv")
+console.log(listaMaterias)
+if(materias.length > 0) {
+    materias.forEach(mat => {
+        var cardMateria = document.createElement('div')
+        cardMateria.setAttribute('class', 'cardMateria')  
+
+        var titulo = document.createElement('span')
+        titulo.setAttribute('class', 'materiaTitulo')
+        titulo.appendChild(document.createTextNode(mat.nome))     
+
+        var info1 = document.createElement('span')
+        var info2 = document.createElement('span')
+        info1.setAttribute('class', 'infoMateria')
+        info2.setAttribute('class', 'infoMateria')
+        info1.appendChild(document.createTextNode(`Pontuação máxima : ${mat.pontos}`))
+        info2.appendChild(document.createTextNode(`Média : ${mat.media}`))  
+
+        cardMateria.appendChild(titulo)
+        cardMateria.appendChild(info1)
+        cardMateria.appendChild(info2)        
+
+        listaMaterias.appendChild(cardMateria)
+    });
+}
+
 function salvar() {
     let nome = document.getElementById("nome").value
     let pontos = document.getElementById("pontos").value
