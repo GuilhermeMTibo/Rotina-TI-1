e619046f8793e64a9b87d665854aa2689a56be1f
GuilhermeMTibo <gmtibo@sga.pucminas.br>
2023-06-01 21:36:29 -0300

    quase finalizando a lógica de estudo e descanso

--- codigo-fonte/src/pomodoro.js
+++ codigo-fonte/src/pomodoro.js
@@ -1,10 +1,72 @@
-var intervaloEstudo; // variável para armazenar o intervalo de estudo
-var intervaloDescanso; // variável para armazenar o intervalo de descanso
-var timerEmAndamento = false; // Variável para controlar se o timer está em andamento
-var duracaoEstudo = 60 * 60; // Duração do estudo em segundos
-var tempoRestante = duracaoEstudo; // Tempo restante do timer, inicialmente igual à duração
+var intervaloEstudo;
+var intervaloDescanso;
+var timerEmAndamento = false;
+var duracaoEstudo = 60 * 0.1; // Duração do estudo em segundos (1 minuto)
+var duracaoDescanso = 60 * 0.15; // Duração do descanso em segundos (10 minutos)
+var tempoRestante = duracaoEstudo; // Tempo restante do timer
 
-function comecaTimer(duracao, display) {
+// Função para iniciar o timer de estudo
+function iniciarEstudo() {
+    var displayEstudo = document.querySelector("#timerEstudo");
+    var botaoStart = document.querySelector("#startButton");
+    var imagemStart = document.querySelector("#startImage");
+
+    if (!timerEmAndamento) {
+        intervaloEstudo = comecaTimer(tempoRestante, displayEstudo, function() {
+            // Callback executado ao terminar o timer de estudo
+            pausarTimer(displayEstudo);
+            iniciarDescanso();
+        });
+
+        timerEmAndamento = true;
+        imagemStart.src = "../assets/Pause.png";
+        imagemStart.alt = "Pause";
+    } else {
+        pausarTimer(displayEstudo);
+    }
+}
+
+// Função para iniciar o timer de descanso
+function iniciarDescanso() {
+    var displayDescanso = document.querySelector("#timerEstudo");
+    var botaoStart = document.querySelector("#startButton");
+    var imagemStart = document.querySelector("#startImage");
+
+    if (!timerEmAndamento) {
+        intervaloDescanso = comecaTimer(duracaoDescanso, displayDescanso, function() {
+            // Callback executado ao terminar o timer de descanso
+            pausarTimer(displayDescanso);
+            iniciarEstudo(); // Iniciar o tempo de estudo após o tempo de descanso
+        });
+
+        timerEmAndamento = true;
+        imagemStart.src = "../assets/Pause.png";
+        imagemStart.alt = "Pause";
+    } else {
+        pausarTimer(displayDescanso);
+    }
+}
+
+// Função para pausar o timer
+function pausarTimer(display) {
+    if (timerEmAndamento) {
+        clearInterval(intervaloEstudo);
+        clearInterval(intervaloDescanso);
+        timerEmAndamento = false;
+
+        var minutos = parseInt(display.textContent.split(":")[0]);
+        var segundos = parseInt(display.textContent.split(":")[1]);
+
+        tempoRestante = minutos * 60 + segundos;
+
+        var imagemStart = document.querySelector("#startImage");
+        imagemStart.src = "../assets/Start.png";
+        imagemStart.alt = "Start";
+    }
+}
+
+// Função para iniciar o timer
+function comecaTimer(duracao, display, callback) {
     var timer = duracao;
     var minutos, segundos;
 
@@ -18,47 +80,10 @@ function comecaTimer(duracao, display) {
         display.textContent = minutos + ":" + segundos;
 
         if (--timer < 0) {
-            timer = duracao;
+            clearInterval(intervalo);
+            callback(); // Chamar o callback ao terminar o timer
         }
     }, 1000);
 
     return intervalo;
-}
-
-function iniciarEstudo() {
-    var displayEstudo = document.querySelector("#timerEstudo"); // elemento para exibir o timer
-    var botaoStart = document.querySelector("#startButton"); // elemento do botão
-    var imagemStart = document.querySelector("#startImage"); // elemento da imagem do botão
-
-    if (!timerEmAndamento) {
-        intervaloEstudo = comecaTimer(tempoRestante, displayEstudo); // inicia o timer com o tempo restante
-
-        timerEmAndamento = true;
-        imagemStart.src = "../assets/Pause.png"; // Altera a imagem do botão para Pause
-        imagemStart.alt = "Pause";
-    } else {
-        pausarTimer(displayEstudo); // Pausa o timer de estudo
-    }
-}
-
-function iniciarDescanso() {
-    var duracaoDescanso = 10 * 60; // conversão para segundos
-    var displayDescanso = document.querySelector("#timerDescanso"); // elemento para exibir o timer
-    intervaloDescanso = comecaTimer(duracaoDescanso, displayDescanso); // Inicia o timer de descanso
-}
-
-function pausarTimer(display) {
-    if (timerEmAndamento) {
-        clearInterval(intervaloEstudo); // Pausa o timer de estudo
-        timerEmAndamento = false;
-
-        // Armazena o tempo restante do timer
-        var minutos = parseInt(display.textContent.split(":")[0]);
-        var segundos = parseInt(display.textContent.split(":")[1]);
-        tempoRestante = minutos * 60 + segundos;
-
-        var imagemStart = document.querySelector("#startImage"); // elemento da imagem do botão
-        imagemStart.src = "../assets/Start.png"; // Altera a imagem do botão para Start
-        imagemStart.alt = "Start";
-    }
 }
