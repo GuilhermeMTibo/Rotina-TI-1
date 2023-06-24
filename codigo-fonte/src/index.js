fbe23e3536684761269fdb565212934079f46962
LAPTOP-ETTN47OT\joaol <joaolucasdv@gmail.com>
2023-06-24 11:06:25 -0300

    modaldeVisualizacao

--- codigo-fonte/src/index.js
+++ codigo-fonte/src/index.js
@@ -8,8 +8,14 @@ for(let j = 0; j<rotinas.length; j++)
     let retanguloSemana = document.getElementById(`semana-${rotinas[j].diaSemana}`)
     let innerRetangle = document.createElement('div')
     innerRetangle.setAttribute('class', 'inner-rectangle')
+    innerRetangle.setAttribute('id', `card-${rotinas[j].id}`)
+    innerRetangle.setAttribute('onclick', `abrirVisualizacao(${rotinas[j].id})`)
     let nomeRotinaSpan = document.createElement('span')
     nomeRotinaSpan.appendChild(document.createTextNode(rotinas[j].nome))
+    let excluirRotina = document.createElement("button")
+    excluirRotina.setAttribute("onclick", `excluirRotina(${rotinas[j].id})`)
+    excluirRotina.appendChild(document.createTextNode("X"))
+    innerRetangle.appendChild(excluirRotina)
     innerRetangle.appendChild(nomeRotinaSpan)
     retanguloSemana.appendChild(innerRetangle)
   }
@@ -42,12 +48,43 @@ function salvarRotina() {
   let retanguloSemana = document.getElementById(`semana-${diaSemanaSelecionado}`)
   let innerRetangle = document.createElement('div')
   innerRetangle.setAttribute('class', 'inner-rectangle')
+  innerRetangle.setAttribute('id', `card-${rot.id}`)
+  innerRetangle.setAttribute('onclick', `abrirVisualizacao(${rot.id})`)
   let nomeRotinaSpan = document.createElement('span')
   nomeRotinaSpan.appendChild(document.createTextNode(rot.nome))
+  let excluirRotina = document.createElement("button")
+  excluirRotina.setAttribute("onclick", `excluirRotina(${rot.id})`)
+  excluirRotina.appendChild(document.createTextNode("X"))
+  innerRetangle.appendChild(excluirRotina)
   innerRetangle.appendChild(nomeRotinaSpan)
   retanguloSemana.appendChild(innerRetangle)
 
   window.location.href = '#'
+}
+
+function excluirRotina(id)
+{
+  let rot = rotinas.filter(r=> r.id ===id)[0]
+  rotinas = rotinas.filter(r=> r.id !==id)
+  localStorage.setItem("rotinas", JSON.stringify(rotinas))
+  let retanguloSemana = document.getElementById(`semana-${rot.diaSemana}`)
+  retanguloSemana.removeChild(document.getElementById(`card-${rot.id}`))
+  window.location.href = "#"
+}
+
+function abrirVisualizacao(id)
+{
+  let elemento = rotinas.filter(r=> r.id===id)[0]
+  window.location.href = '#modalVisualizacao'
+  let tituloArea = document.getElementById("tituloArea")
+  if(document.getElementById("visualizacaoTitulo")) {
+    tituloArea.removeChild(document.getElementById("visualizacaoTitulo"))
+  }
+  let titulo = document.createElement("span")
+  titulo.setAttribute("class", "tituloVisualizacao")
+  titulo.setAttribute("id", "visualizacaoTitulo")
+  titulo.appendChild(document.createTextNode(elemento.nome))
+  tituloArea.appendChild(titulo)
 }
 
 function salvarNomeObjetivo() {
